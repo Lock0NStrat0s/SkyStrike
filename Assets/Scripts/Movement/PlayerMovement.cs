4ca8d18afb281487a39d731af12e51f5f5b87b5f
Mohammad Jabir <mohammadj94@gmail.com>
2023-04-12 02:58:52 -0600

    fully revamped movement system that uses mouse left click and drag to rotate plane. also implemented sprite flip when plane is facing left or right direction. need to have plane rotate to mouse click instead of suddenly changing its transform

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,12 @@ public class PlayerMovement : MonoBehaviour
  [SerializeField] float turnSpeed = 5f;             //controler turn speed of plane
  [SerializeField] GameObject bullet;
  [SerializeField] Transform gun;
+ [SerializeField] Camera mainCam;
  Vector2 moveInput;                                 //get input from user
  Rigidbody2D myRigidbody;
  CapsuleCollider2D myBodyCollider;
-
+ bool hasFlippedRight = false;
+ bool hasFlippedLeft = true;
  bool isAlive = true;
  void Start()
  {
@@ -22,26 +25,56 @@ public class PlayerMovement : MonoBehaviour
 
  void Update()
  {
-
   Fly();
- }
+  FlipSprite();
+  IsMouseBeingHeld();
 
- void OnMove(InputValue value)
- {
-  moveInput = value.Get<Vector2>();
  }
-
  void Fly()
  {
   float moveAmount = moveSpeed * Time.deltaTime;
-  float turnAmount = -moveInput.x * turnSpeed * Time.deltaTime;
-
   transform.Translate(-moveAmount, 0, 0);
-  transform.Rotate(0, 0, turnAmount);
  }
 
- void OnFire(InputValue value)
+ void FlipSprite()
+ {
+  var orientation = transform.rotation.z * Mathf.Rad2Deg * 2;
+  Debug.Log(orientation);
+
+  if (((orientation > 90 && orientation < 180) || (orientation > -180 && orientation < -90)) && !hasFlippedRight)
+  {
+   hasFlippedRight = true;
+   hasFlippedLeft = false;
+   transform.localScale = new Vector2(1f, -transform.localScale.y);
+  }
+  else if ((orientation > -90 && orientation < 90) && !hasFlippedLeft)
+  {
+   hasFlippedRight = false;
+   hasFlippedLeft = true;
+   transform.localScale = new Vector2(1f, -transform.localScale.y);
+  }
+ }
+
+ void OnShoot(InputValue value)
  {
   Instantiate(bullet, gun.position, transform.rotation);
+ }
+
+ void OnClick(InputValue value)
+ {
+  RotateToMouse();
+ }
+ private void IsMouseBeingHeld()
+ {
+  if (Mouse.current.leftButton.isPressed)
+  {
+   RotateToMouse();
+  }
+ }
+ private void RotateToMouse()
+ {
+  Vector2 turn = (transform.position - mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+  float angle = Mathf.Atan2(turn.y, turn.x) * Mathf.Rad2Deg;
+  transform.rotation = Quaternion.Euler(0, 0, angle);
  }
 }
